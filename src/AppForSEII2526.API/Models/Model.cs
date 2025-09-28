f10a22d6631a6d3149049e5c501efa2f56976716
PabloRamonPalarea <pablo.ramon@alu.uclm.es>
2025-09-28 19:22:52 +0200

    Corrección de las clases model y car (#11)
    Correción de las clases model y car

---
+++ src/AppForSEII2526.API/Models/Model.cs
@@ -0,0 +1,13 @@
+using System;
+
+public class Model
+{
+    public Model(string name)
+    {
+        Name = name;
+    }
+
+    public int Id { get; set; }
+    public string Name { get; set; }
+
+}
