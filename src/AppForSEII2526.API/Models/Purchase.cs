b9a85dd4f5d8fa8fb31de99c1fb1c67fca05541e
TOMÁS GONZÁLEZ ATIENZAR <Tomas.Gonzalez2@alu.uclm.es>
2025-10-01 16:28:50 +0200

    Añadidos los atributos a cada clase de Comprar Coche

--- src/AppForSEII2526.API/Models/Purchase.cs
+++ src/AppForSEII2526.API/Models/Purchase.cs
@@ -2,11 +2,37 @@ namespace AppForSEII2526.API.Models
 {
     public class Purchase
     {
+        public enum PaymentMethodTypes
+        {//Metodos de pago}
+            Efectivo,
+            TarjetaCredito,
+            TarjetaDebito,
+            TransferenciaBancaria,
+            PagoMovil,
+            PayPal,
+            Criptomoneda,
+            Cheque
 
+        }
         public Purchase()
         {
 
         }
+
+        public Purchase(int id, PaymentMethodTypes paymentMethod, DateTime purchasingDate, decimal purchasingPrice, bool deliveryCarDealer)
+        {
+            Id = id;
+            PaymentMethod = paymentMethod;
+            PurchasingDate = purchasingDate;
+            PurchasingPrice = purchasingPrice;
+            DeliveryCarDealer = deliveryCarDealer;
+        }
+
+        public int Id { get; set; }
+        public PaymentMethodTypes PaymentMethod { get; set; }
+        public DateTime PurchasingDate { get; set; }
+        public decimal PurchasingPrice { get; set; }
+        public bool DeliveryCarDealer { get; set; }
 
     }
 
