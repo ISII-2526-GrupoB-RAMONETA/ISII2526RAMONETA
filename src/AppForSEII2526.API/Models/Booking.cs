759b80aee02e07a8c8139eedf91ae1287d0511de
PabloRamonPalarea <pablo.ramon@alu.uclm.es>
2025-10-02 09:35:12 +0200

    Atributos añadidos al modelo de clases para Contratar Mantenimiento (#22)
    * Atributos añadidos al modelo de clases para Contratar Mantenimiento
    
    * Maintenance.cs

--- src/AppForSEII2526.API/Models/Booking.cs
+++ src/AppForSEII2526.API/Models/Booking.cs
@@ -2,5 +2,35 @@
 {
     public class Booking
     {
+        public Booking()
+        {
+        }
+
+        public Booking(string clientAdress, string clientName, string clientPhoneNumber, string clientSurname, DateTime Date, string PaymentMethod)
+        {
+            clientAdress = clientAdress;
+            clientName = clientName;
+            clientPhoneNumber = clientPhoneNumber;
+            clientSurname = clientSurname;
+            Date = Date;
+            PaymentMethod = PaymentMethod;
+        }
+
+        public int Id { get; set; }
+
+        public string ClientAdress { get; set; }
+
+        public string ClientName { get; set; }
+
+        public string ClientPhoneNumber { get; set; }
+
+        public string ClientSurname { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public string PaymentMethod { get; set; }
+
+
+
     }
 }
--- src/AppForSEII2526.API/Models/Maintenance.cs
+++ src/AppForSEII2526.API/Models/Maintenance.cs
@@ -1,6 +1,26 @@
-﻿namespace AppForSEII2526.API.Models
+namespace AppForSEII2526.API.Models
 {
     public class Maintenance
     {
+        public Maintenance()
+        {
+        }
+
+        public Maintenance(string name,int NumberOfDays,float Price) { 
+        name = name;
+        NumberOfDays = NumberOfDays;
+        Price = Price;
+        }
+
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int NumberOfDays { get; set; }
+
+        public float Price { get; set; }
+
+
+
     }
 }
