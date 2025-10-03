423948b4c44b7906e5aaf923a7c565b5c10bb87b
TOMÁS GONZÁLEZ ATIENZAR <Tomas.Gonzalez2@alu.uclm.es>
2025-10-03 13:41:34 +0200

    Relaciones creadas entre PurchaseItem y Purchase y entre PurchaseItem y Car.

--- src/AppForSEII2526.API/Models/PurchaseItem.cs
+++ src/AppForSEII2526.API/Models/PurchaseItem.cs
@@ -1,8 +1,9 @@
 namespace AppForSEII2526.API.Models
 {
+    [PrimaryKey(nameof(CarId), nameof(PurchaseId))]
     public class PurchaseItem
     {
-
+         
         public PurchaseItem()
         {
 
@@ -14,9 +15,18 @@ namespace AppForSEII2526.API.Models
             Quantity = quantity;
         }
 
+
+        
+        public Car Car { get; set; }
         public int CarId { get; set; }
+
+        public Purchase Purchase { get; set; }
         public int PurchaseId { get; set; }
+
+
+
         public int Quantity { get; set; }
+
 
 
     }
