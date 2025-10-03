5ccd1dc1f383c7348b3289a57453703ed15c79b0
Tomás González <tomas.gonzalez2@alu.uclm.es>
2025-10-03 18:54:55 +0200

    Creada restricción al atributo Name en la clase Model (#39)
    Creada restricción al atributo Name

--- src/AppForSEII2526.API/Models/Model.cs
+++ src/AppForSEII2526.API/Models/Model.cs
@@ -15,6 +15,8 @@ namespace AppForSEII2526.API.Models
         }
 
         public int Id { get; set; }
+
+        [StringLength(20, ErrorMessage = "El nombre del model no puede tener más de 20 caracteres ni menos de 1.", MinimumLength=1)]
         public string Name { get; set; }
 
         public IList<Car> Cars { get; set; } 
