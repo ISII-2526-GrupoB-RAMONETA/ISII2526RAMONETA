5a2701227e6b83869b6d6777ddf959cedc61f93d
PABLO RAMÓN PALAREA <Pablo.Ramon@alu.uclm.es>
2025-10-03 12:20:55 +0200

    Eliminación de un punto y coma erróneo

--- src/AppForSEII2526.API/Models/Model.cs
+++ src/AppForSEII2526.API/Models/Model.cs
@@ -14,7 +14,7 @@ namespace AppForSEII2526.API.Models
                        Name = name;
         }
 
-        public int Id { get; set; };
+        public int Id { get; set; }
         public string Name { get; set; }
     }
 }
