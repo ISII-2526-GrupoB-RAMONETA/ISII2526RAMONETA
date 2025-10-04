21c9ba16416984f9f062f5e1a5d3aa8dd429de44
PABLO BALLESTERO TORRES <Pablo.Ballestero@alu.uclm.es>
2025-10-04 14:00:37 +0200

    Creada relación una clase RentalItem tiene una clase Car

--- src/AppForSEII2526.API/Models/RentalItem.cs
+++ src/AppForSEII2526.API/Models/RentalItem.cs
@@ -10,11 +10,11 @@
 
         public RentalItem(int carId, int rentalId, int quantity)
         {
-            CarId = carId;
             RentalId = rentalId;
             Quantity = quantity;
         }
 
+        public Car Car { get; set; }
         public int CarId { get; set; }
         public int RentalId { get; set; }
         public int Quantity { get; set; }
