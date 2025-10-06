8337f6ec727ffb17fb5e5a0af5093c1983e03549
PabloBallestero <Pablo.Ballestero@alu.uclm.es>
2025-10-06 18:24:36 +0200

    Creadas restricciones de los atributos de la clase RentalItem (#84)
    Creadas restricciones de los atributos de la clase RentalItem (#84)

--- src/AppForSEII2526.API/Models/RentalItem.cs
+++ src/AppForSEII2526.API/Models/RentalItem.cs
@@ -1,5 +1,6 @@
 ﻿namespace AppForSEII2526.API.Models
 {
+    [PrimaryKey(nameof(CarId), nameof(RentalId))]
     public class RentalItem
     {
 
@@ -18,6 +19,8 @@
         public int CarId { get; set; }
         public Rental Rental { get; set; }
         public int RentalId { get; set; }
+
+        [Range(1, int.MaxValue, ErrorMessage = "You must provide a quantity higher than 1")]
         public int Quantity { get; set; }
 
 
