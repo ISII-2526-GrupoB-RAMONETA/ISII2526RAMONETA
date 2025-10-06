353299c0aa2b7c2cbd6e996d7829f923dbcb5b78
PabloRamonPalarea <pablo.ramon@alu.uclm.es>
2025-10-06 18:12:02 +0200

    Añadidas restricciones a los atributos de la clase Maintenance (#82)

--- src/AppForSEII2526.API/Models/Maintenance.cs
+++ src/AppForSEII2526.API/Models/Maintenance.cs
@@ -14,10 +14,14 @@ namespace AppForSEII2526.API.Models
 
         public int Id { get; set; }
 
+        [StringLength(50, ErrorMessage = "Name cannot be longer than 50 characters.")]
         public string Name { get; set; }
 
+        [Display(Name = "Number of days")]
+        [Range(1, int.MaxValue, ErrorMessage = "Minimum days for maintenance is 1")]
         public int NumberOfDays { get; set; }
 
+        [Precision(10, 2)]
         public float Price { get; set; }
 
         public IList<MaintenanceType> MaintenanceTypes { get; set; }
