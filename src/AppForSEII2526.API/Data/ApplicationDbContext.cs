a1628fcc8c4249b34ab6953c4cd177735b362a86
PabloRamonPalarea <pablo.ramon@alu.uclm.es>
2025-10-08 17:07:14 +0200

    Añadidas clases al fichero ApplicationDbContext.cs (#108)
    Añadidas clases al fichero ApplicationDbContext.cs

--- src/AppForSEII2526.API/Data/ApplicationDbContext.cs
+++ src/AppForSEII2526.API/Data/ApplicationDbContext.cs
@@ -5,4 +5,19 @@ using AppForSEII2526.API.Models;
 namespace AppForSEII2526.API.Data;
 
 public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options) {
+
+    public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+    public DbSet<Booking> Bookings { get; set; }
+    public DbSet<BookingItem> BookingItems { get; set; }
+    public DbSet<Car> Cars { get; set; }
+    public DbSet<Maintenance> Maintenances { get; set; }
+    public DbSet<MaintenanceType> MaintenanceTypes { get; set; }
+    public DbSet<Model> Models { get; set; }
+    public DbSet<Purchase> Purchases { get; set; }
+    public DbSet<PurchaseItem> PurchaseItems { get; set; }
+    public DbSet<Rental> Rentals { get; set; }
+    public DbSet<RentalItem> RentalItems { get; set; }
+
+
+
 }
