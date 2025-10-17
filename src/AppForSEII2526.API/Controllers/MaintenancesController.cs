ae2a7245cf530caea7a554ea76c1532d45fa54ba
PABLO RAMÓN PALAREA <Pablo.Ramon@alu.uclm.es>
2025-10-17 17:56:16 +0200

    Creación del controlador y DTO  de Maintenance con el método get simple

---
+++ src/AppForSEII2526.API/Controllers/MaintenancesController.cs
@@ -0,0 +1,61 @@
+﻿using AppForSEII2526.API.DTOs.MaintenancesDTO;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AppForSEII2526.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MaintenancesController : ControllerBase
+    {
+        //used to enable your controller to acces to the database
+        private readonly ApplicationDbContext _context;
+        //used to log any information when your system is running
+        private readonly ILogger<MaintenancesController> _logger;
+
+
+
+
+
+        //Método GET que causa error en el Swagger 
+
+        //[HttpGet]
+        //[Route("[action]")]
+        //[ProducesResponseType(typeof(IList<Maintenance>), (int)HttpStatusCode.OK)]
+
+        //public async Task<ActionResult> GetMaintenances()
+        //{
+        //   IList<Maintenance> maintenances = await _context.Maintenances
+        //        .ToListAsync();
+        //    return Ok(maintenances);
+
+        //}
+
+
+
+
+
+        [HttpGet]
+        [Route("[action]")]
+        [ProducesResponseType(typeof(IList<MaintenanceDTO>), (int)HttpStatusCode.OK)]
+
+        public async Task<ActionResult> GetMaintenances()
+        {
+            var maintenances = await _context.Maintenances
+                .Select(m => new MaintenanceDTO(m.Id, m.Name, m.MaintenanceType.Type)).ToListAsync();
+            return Ok(maintenances);
+
+        }
+
+
+
+
+
+
+
+    }
+
+
+
+
+}
---
+++ src/AppForSEII2526.API/DTOs/MaintenancesDTO/MaintenanceDTO.cs
@@ -0,0 +1,20 @@
+﻿namespace AppForSEII2526.API.DTOs.MaintenancesDTO
+{
+    public class MaintenanceDTO
+    {
+
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Type { get; set; }
+
+        public MaintenanceDTO(int id, string name, string type)
+        {
+            Id = id;
+            Name = name;
+            Type = type;
+        }
+    }
+}
+    
