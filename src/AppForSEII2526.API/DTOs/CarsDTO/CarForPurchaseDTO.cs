1d141815a0109a6fcbfb9a5e94b83913fc1ebdc6
TOMÁS GONZÁLEZ ATIENZAR <Tomas.Gonzalez2@alu.uclm.es>
2025-10-23 08:21:51 +0200

    Añadido el DTO de car y el controller

---
+++ src/AppForSEII2526.API/DTOs/CarsDTO/CarForPurchaseDTO.cs
@@ -0,0 +1,32 @@
+﻿
+using System.Drawing;
+
+namespace AppForSEII2526.API.DTOs.CarsDTO
+{
+    public class CarForPurchaseDTO
+    {
+        //private Model model;
+
+        public CarForPurchaseDTO(int id, string model, string color, string fueltype, string manufacturer, decimal purchasingPrice)
+        {
+            Id = id;
+            Model = model;
+            Color = color;
+            Fueltype = fueltype;
+            Manufacturer = manufacturer;
+            PurchasingPrice = purchasingPrice;
+        }
+
+        public int Id { get; set; }
+        
+        public string Color { get; set; }
+
+        public string Model { get; set; }
+
+        public string Fueltype { get; set; }
+
+        public string Manufacturer { get; set; }
+
+        public decimal PurchasingPrice { get; set; }
+    }
+}
