7a4954446046f9568d8ec50514d11078dfedf380
PabloRamonPalarea <pablo.ramon@alu.uclm.es>
2025-10-24 11:20:48 +0200

    Creado método GET de detalles para las clases Booking (#134)

---
+++ src/AppForSEII2526.API/DTOs/MaintenancesDTO/BookingForCreateDTO.cs
@@ -0,0 +1,45 @@
+﻿using AppForSEII2526.API.Models;
+
+namespace AppForSEII2526.API.DTOs.MaintenancesDTO
+{
+    public class BookingForCreateDTO
+    {
+        public string CustomerUserName { get; set; }
+
+        public string CustomerUserSurname { get; set; }
+
+        public string Address { get; set; }
+
+        public PaymentMethodTypes PaymentMethod { get; set; }
+
+
+        public IList<BookingItemDTO> BookingItems { get; set; }
+
+        public decimal TotalPrice
+        {
+            get
+            {
+                return BookingItems.Sum(ri => ri.Price * ri.NumberOfDays);
+            }
+        }
+
+        public int NumberOfDays { get
+            {
+                return BookingItems.Sum(ri => ri.NumberOfDays);
+            }
+        }
+
+        public BookingForCreateDTO(string customerUserName, string customerUserSurname, string address, PaymentMethodTypes paymentMethod, IList<BookingItemDTO> bookingItems)
+        {
+            CustomerUserName = customerUserName;
+            CustomerUserSurname = customerUserSurname;
+            Address = address;
+            PaymentMethod = paymentMethod;
+            BookingItems = bookingItems;
+        }
+
+        public BookingForCreateDTO() { 
+        BookingItems= new List<BookingItemDTO>();   
+        }
+    }
+}
