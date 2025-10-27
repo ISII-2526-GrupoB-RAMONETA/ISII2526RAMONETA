306ed727b0304fe89fa1ab0a477101fd9f83afbc
PabloBallestero <Pablo.Ballestero@alu.uclm.es>
2025-10-27 10:16:36 +0100

    Creados los DTOs de Rental y RentalsController

---
+++ src/AppForSEII2526.API/DTOs/RentalDTOs/RentalItemDTO.cs
@@ -0,0 +1,24 @@
+﻿namespace AppForSEII2526.API.DTOs.RentalDTO
+{
+    public class RentalItemDTO
+    {
+        public int CarId { get; set; }
+
+        public string Model { get; set; }
+
+        public string Manufacturer { get; set; }
+
+        public decimal Price { get; set; }
+
+        public int Quantity { get; set; }
+
+        public RentalItemDTO(int carId, string model, string manufacturer, decimal price, int quantity)
+        {
+            CarId = carId;
+            Model = model;
+            Manufacturer = manufacturer;
+            Price = price;
+            Quantity = quantity;
+        }
+    }
+}
