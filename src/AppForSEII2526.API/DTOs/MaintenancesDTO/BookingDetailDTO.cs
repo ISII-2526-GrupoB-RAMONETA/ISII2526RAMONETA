37f9cac4412aebb95462caaab3e8e3e5de377978
PABLO RAMÓN PALAREA <Pablo.Ramon@alu.uclm.es>
2025-11-10 20:05:39 +0100

    Creadas pruebas unitarias para el método de ver el detalle de una reserva

--- src/AppForSEII2526.API/DTOs/MaintenancesDTO/BookingDetailDTO.cs
+++ src/AppForSEII2526.API/DTOs/MaintenancesDTO/BookingDetailDTO.cs
@@ -1,4 +1,7 @@
-﻿namespace AppForSEII2526.API.DTOs.MaintenancesDTO
+﻿
+using AppForSEII2526.API.Models;
+
+namespace AppForSEII2526.API.DTOs.MaintenancesDTO
 {
     public class BookingDetailDTO : BookingForCreateDTO
     {
@@ -7,7 +10,7 @@
         public DateTime Date { get; set; }
 
         public BookingDetailDTO(int id, DateTime date, string customerUserName,string customerName, string customerSurname,
-            string address, PaymentMethodTypes paymentMethod, string phoneNumber, IList<BookingItemDTO> bookingItems)
+            string address, PaymentMethodTypes paymentMethod, string? phoneNumber, IList<BookingItemDTO> bookingItems)
         : base(customerUserName,customerName, customerSurname, address,
               paymentMethod,
               phoneNumber,
@@ -20,7 +23,37 @@
 
         }
 
+        public override bool Equals(object? obj)
+        {
+            return obj is BookingDetailDTO dTO &&
+                   CustomerUserName == dTO.CustomerUserName &&
+                   CustomerName == dTO.CustomerName &&
+                   CustomerSurname == dTO.CustomerSurname &&
+                   Address == dTO.Address &&
+                   PaymentMethod == dTO.PaymentMethod &&
+                   PhoneNumber == dTO.PhoneNumber &&
+                   BookingItems.SequenceEqual(dTO.BookingItems) &&
+                   TotalPrice == dTO.TotalPrice &&
+                   TotalNumberOfDays == dTO.TotalNumberOfDays &&
+                   Id == dTO.Id &&
+                   Date == dTO.Date;
+        }
 
-
+        public override int GetHashCode()
+        {
+            HashCode hash = new HashCode();
+            hash.Add(CustomerUserName);
+            hash.Add(CustomerName);
+            hash.Add(CustomerSurname);
+            hash.Add(Address);
+            hash.Add(PaymentMethod);
+            hash.Add(PhoneNumber);
+            hash.Add(BookingItems);
+            hash.Add(TotalPrice);
+            hash.Add(TotalNumberOfDays);
+            hash.Add(Id);
+            hash.Add(Date);
+            return hash.ToHashCode();
+        }
     }
 }
