46ae372838c3acc48282a2e408d4d892bb030e3c
PABLO RAMÓN PALAREA <Pablo.Ramon@alu.uclm.es>
2025-11-11 09:24:33 +0100

    Corregido fallo en la migración

--- src/AppForSEII2526.API/Migrations/20251111082244_CreateIdentitySchema.cs
+++ src/AppForSEII2526.API/Migrations/20251111082244_CreateIdentitySchema.cs
@@ -193,7 +193,9 @@ namespace AppForSEII2526.API.Migrations
                         .Annotation("SqlServer:Identity", "1, 1"),
                     Date = table.Column<DateTime>(type: "datetime2", nullable: false),
                     PaymentMethod = table.Column<int>(type: "int", nullable: false),
-                    ApplicationUserId = table.Column<string>(type: "nvarchar(450)", nullable: false)
+                    ApplicationUserId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    TotalPrice = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    TotalNumberOfDays = table.Column<int>(type: "int", nullable: false)
                 },
                 constraints: table =>
                 {
