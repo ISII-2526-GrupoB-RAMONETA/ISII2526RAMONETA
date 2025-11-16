22cd2ac926d2b69ca00ed5729389e3c6c5d09ec5
PabloBallestero <Pablo.Ballestero@alu.uclm.es>
2025-11-16 12:33:20 +0100

    Añadir cambios de development a rama task174-crear-pruebas-unitarias-para-el-metodo-post-de-alquilar-coche (#177)
    * Creado pruebas de detalle de alquilar coches (creada GetRentals_test.cs) (#172)
    
    * Creado pruebas de detalle de alquilar coches (creada GetRentals_test.cs)
    
    * correción en la estructura de GetRentals_test.cs
    
    * Se han creado las pruebas del método post de reservas (#173)
    
    Se han creado las pruebas del método post y se han corregido errores en otras clases
    
    * Corregida migración y añadido script para la base de datos (#176)
    
    ---------
    
    Co-authored-by: PabloRamonPalarea <pablo.ramon@alu.uclm.es>

---
+++ test/AppForSEII2526.UT/BookingsController_test/PostBookings_test.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AppForSEII2526.API.DTOs;
+using AppForSEII2526.API.DTOs.MaintenancesDTO;
+using AppForSEII2526.API.Controllers;
+
+namespace AppForSEII2526.UT.BookingsController_test
+{
+    public class PostBookings_test : AppForSEII25264SqliteUT
+    {
+        private const string _userName = "pablo.ramon@uclm.es";
+        private const string _customerName = "Pablo";
+        private const string _customerSurname = "Ramón Palarea";
+        private const string _address = "Val general";
+        private const string _phoneNumber = "622";
+
+        private const string _maintenance1Name = "Air filter cleaning";
+        private const string _maintenance1Type = "Preventive";
+        private const string _maintenance2Name = "Repair ventilation motor";
+        private const string _maintenance2Type = "Corrective";
+
+        public PostBookings_test()
+        {
+            var types = new List<MaintenanceType>()
+            {
+                new MaintenanceType (_maintenance1Type),
+                new MaintenanceType (_maintenance2Type)
+            };
+
+            var maintenances = new List<Maintenance>()
+            {
+                new Maintenance(_maintenance1Name,3,80m,types[0]),
+                new Maintenance(_maintenance2Name,5,300m,types[1])
+            };
+
+            ApplicationUser user = new ApplicationUser("1", _customerName, _customerSurname, _userName, _address, _phoneNumber);
+
+            var booking = new Booking(DateTime.Now, PaymentMethodTypes.Efectivo, user, new List<BookingItem>());
+            booking.BookingItems.Add(new BookingItem(booking, "Air filter cleaned, dust and debris removed", maintenances[0]));
+
+            _context.ApplicationUsers.Add(user);
+            _context.AddRange(types);
+            _context.AddRange(maintenances);
+            _context.Add(booking);
+            _context.SaveChanges();
+
+
+        }
+
+        public static IEnumerable<object[]> TestCasesFor_CreateBooking()
+        {
+            var bookingNoItem = new BookingForCreateDTO(_userName, _customerName, _customerSurname, _address, PaymentMethodTypes.Efectivo, _phoneNumber, new List<BookingItemDTO>());
+
+            var bookingItems = new List<BookingItemDTO>()
+            {
+                new BookingItemDTO(2,_maintenance2Name,5,300m,"Ventilation system repaired, main fan replaced",_maintenance2Type)
+
+            };
+
+            var BookingApplicationUser = new BookingForCreateDTO("usuario.ejemplo@uclm.es", _customerName, _customerSurname, _address, PaymentMethodTypes.Efectivo, _phoneNumber, bookingItems);
+
+            var bookingMaintenanceNotAvaible = new BookingForCreateDTO(_userName, _customerName, _customerSurname, _address,
+                PaymentMethodTypes.Efectivo, _phoneNumber, new List<BookingItemDTO>() {new BookingItemDTO(0, "Mantenimiento nulo",0,0,"0", "") });
+
+
+            var allTests = new List<object[]>
+            {
+                new object []{ bookingNoItem, "Error! You must include at least one maintenance to be booked", },
+                new object []{ BookingApplicationUser, "Error! UserName is not registered", },
+                new object []{ bookingMaintenanceNotAvaible, "Error! Maintenance named 'Mantenimiento nulo' is not available for being booked", },
+
+
+            };
+
+            return allTests;
+
+
+        }
+
+        [Theory]
+        [Trait("LevelTesting", "Unit Testing")]
+        [Trait("Database", "WithoutFixture")]
+        [MemberData(nameof(TestCasesFor_CreateBooking))]
+        public async Task CreateBooking_Error_test(BookingForCreateDTO bookingDTO, string errorExpected)
+        {
+            //Arrange
+            var mock = new Mock<ILogger<BookingsController>>();
+            ILogger<BookingsController> logger = mock.Object;
+
+            var controller = new BookingsController(_context, logger);
+
+            //Act
+            var result = await controller.CreateBooking(bookingDTO);
+
+            //Assert
+            //we check that the response type is BadRequest and obtain the error returned
+            var badRequestResult = Assert.IsType<BadRequestObjectResult>(result);
+            var problemDetails = Assert.IsType<ValidationProblemDetails>(badRequestResult.Value);
+
+            var errorActual = problemDetails.Errors.First().Value[0];
+
+            //we check that the expected error message and actual are the same
+            Assert.StartsWith(errorExpected, errorActual);
+
+
+        }
+
+        [Fact]
+        [Trait("LevelTesting", "Unit Testing")]
+        [Trait("Database", "WithoutFixture")]
+        public async Task CreateBooking_Success_test()
+        {
+            //Arrange
+            var mock = new Mock<ILogger<BookingsController>>();
+            ILogger<BookingsController> logger = mock.Object;
+
+            var controller = new BookingsController(_context, logger);
+
+            var bookingDTO = new BookingForCreateDTO(_userName, _customerName, _customerSurname, _address, PaymentMethodTypes.Efectivo, _phoneNumber,
+                new List<BookingItemDTO>() { new BookingItemDTO(2, _maintenance1Name, 3, 80m, "Air filter cleaned, dust and debris removed", _maintenance1Type) });
+
+
+            var expectedBookingDetailDTO = new BookingDetailDTO(2, DateTime.Now, _userName, _customerName, _customerSurname, _address, PaymentMethodTypes.Efectivo,
+                _phoneNumber,
+                new List<BookingItemDTO>() { new BookingItemDTO(2, _maintenance1Name, 3, 80m, "Air filter cleaned, dust and debris removed", _maintenance1Type) });
+
+
+            //Act
+            var result = await controller.CreateBooking(bookingDTO);
+
+            //Assert
+            //we check that the response type is BadRequest and obtain the error returned
+            var createdResult = Assert.IsType<CreatedAtActionResult>(result);
+            var actualBookingDetailDTO = Assert.IsType<BookingDetailDTO>(createdResult.Value);
+
+            Assert.Equal(expectedBookingDetailDTO, actualBookingDetailDTO);
+
+
+
+
+        }
+
+
+
+
+
+
+
+    }
+}
