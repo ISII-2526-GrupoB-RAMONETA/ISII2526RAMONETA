1c3e9a5fbb0769584f2fdb7511e5374531859c37
PabloRamonPalarea <pablo.ramon@alu.uclm.es>
2025-12-03 19:26:48 +0100

    Actualizar rama de seleccionar mantenimientos (#205)
    * establecida conexión con la api (con localhost) (#202)
    
    * Task197 desarrollar UI select comprarcoche (#203)
    
    * Creado el PurchaseStateContainer.
    
    * Añadido el StateContainer como servicio en Program.cs
    
    * Arreglado error de using
    
    ---------
    
    Co-authored-by: PabloBallestero <Pablo.Ballestero@alu.uclm.es>
    Co-authored-by: Tomás González <tomas.gonzalez2@alu.uclm.es>

---
+++ src/AppForSEII2526.Web/PurchaseStateContainer.cs
@@ -0,0 +1,64 @@
+﻿using AppForSEII2526.Web.API;
+
+namespace AppForSEII2526.Web
+{
+    public class PurchaseStateContainer
+    {
+        //creamos una instancia de Purchase cuando se crea una instancia de RentalState Container
+        public PurchaseForCreateDTO Purchase { get; private set; } = new PurchaseForCreateDTO()
+        {
+            PurchaseItems = new List<PurchaseItemDTO>()
+        };
+
+        public event Action? OnChange;
+
+        private void NotifyStateChanged() => OnChange?.Invoke();
+
+        public void AddCarToPurchase(PurchaseItemDTO purchaseItem)
+        {
+            var existingItem = Purchase.PurchaseItems.FirstOrDefault(pi => pi.CarID == purchaseItem.CarID);
+            //Buscamos si el coche ya está en la lista
+            if (existingItem == null)
+            {
+                //lo agregamos si no está en la lista
+                Purchase.PurchaseItems.Add(new PurchaseItemDTO()
+                {
+                    CarID = purchaseItem.CarID,
+                    Model = purchaseItem.Model,
+                    PurchasingPrice = purchaseItem.PurchasingPrice,
+                    Color = purchaseItem.Color,
+                    Quantity = purchaseItem.Quantity,
+                    Description = purchaseItem.Description
+                }
+                );
+            }
+            else
+            {
+                //si ya está en la lista, aumentamos la cantidad
+                existingItem.Quantity += purchaseItem.Quantity;
+            }
+        }
+
+        //para eliminar un coche del carrito de compras
+        public void RemovePurchaseItemToPurchase(PurchaseItemDTO purchaseItem)
+        {
+            Purchase.PurchaseItems.Remove(purchaseItem);
+        }
+
+        //para vaciar el carrito de compras
+        public void ClearPurchasingCart()
+        {
+            Purchase.PurchaseItems.Clear();
+        }
+
+        //para resetear la compra después de que se haya procesado
+        public void PurchaseProcessed()
+        {
+            Purchase = new PurchaseForCreateDTO()
+            {
+                PurchaseItems = new List<PurchaseItemDTO>()
+            };
+        }
+
+    }
+}
