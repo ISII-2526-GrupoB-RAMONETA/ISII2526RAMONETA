e640c80c0554c26e9a405defc9f864ef53cb0f8d
PABLO RAMÓN PALAREA <Pablo.Ramon@alu.uclm.es>
2025-12-04 23:46:56 +0100

    Creada la interfaz para seleccionar mantenimientos para reservar

---
+++ src/AppForSEII2526.API/Controllers/TypesController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AppForSEII2526.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TypesController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        private ILogger _logger;
+
+        public TypesController(ApplicationDbContext context, ILogger<MaintenancesController> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+
+        // GET: api/Maintenances/GetMaintenancesForBooking
+        [HttpGet]
+        [Route("[action]")]
+        [ProducesResponseType(typeof(IList<string>), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult> GetTypes(string? typeName)
+        {
+
+            IList<string> types = await _context.MaintenanceTypes
+                .Where(type => (typeName == null || type.Type.Contains(typeName))) // where clause             
+                .OrderBy(type => type.Type)
+                .Select(type => type.Type)
+                .ToListAsync();
+
+            return Ok(types);
+        }
+    }
+}
