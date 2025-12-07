364a00c62db366325fedc5df87581894e1e48cc5
TOMÁS GONZÁLEZ ATIENZAR <Tomas.Gonzalez2@alu.uclm.es>
2025-12-07 20:29:40 +0100

    Cambios añadidos

---
+++ src/AppForSEII2526.API/Controllers/ModelsController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore; // <-- Agrega este using
+
+namespace AppForSEII2526.API.Controllers
+{
+    public class ModelsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private ILogger _logger;
+
+        public ModelsController(ApplicationDbContext context, ILogger<CarsController> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+
+        // GET: api/Cars/GetCarsForRental
+        [HttpGet]
+        [Route("[action]")]
+        [ProducesResponseType(typeof(IList<string>), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult> GetModels(string? modelName)
+        {
+
+            IList<string> models = await _context.Models
+                .Where(model => (modelName == null || model.Name.Contains(modelName))) // where clause             
+                .OrderBy(model => model.Name)
+                .Select(model => model.Name)
+                .ToListAsync();
+
+            return Ok(models);
+        }
+    }
+}
