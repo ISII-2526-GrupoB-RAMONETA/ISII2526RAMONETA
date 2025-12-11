2d56d944da7898f1299f1353c16de636f2c39a41
Tomás González <tomas.gonzalez2@alu.uclm.es>
2025-12-11 10:35:39 +0100

    Fix formatting and add car to purchase correctly
    Fix formatting and add car to purchase correctly

--- src/AppForSEII2526.Web/PurchaseStateContainer.cs
+++ src/AppForSEII2526.Web/PurchaseStateContainer.cs
@@ -19,16 +19,17 @@ namespace AppForSEII2526.Web
         public void AddCarToPurchase(CarForPurchaseDTO car)
         {
             if (!Purchase.PurchaseItems.Any(pi => pi.CarID == car.Id))
-                Purchase.PurchaseItems.Add(new PurchaseItemDTO()
+            //we add it if it is not in the list
+            Purchase.PurchaseItems.Add(new PurchaseItemDTO()
             {
-                CarID = car.Id,
-                Model = car.Model,
+                CarID=car.Id,
+                Model =car.Model,
                 PurchasingPrice = car.PurchasingPrice,
                 Color = car.Color,
-                Quantity = 1,
-                Description = car.Description
-            });
-}
+                Quantity = 1
+            }
+            );
+        }   
 
         //para eliminar un coche del carrito de compras
         public void RemovePurchaseItemToPurchase(PurchaseItemDTO purchaseItem)
