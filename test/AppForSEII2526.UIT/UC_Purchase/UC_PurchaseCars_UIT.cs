02e088cdbdd46f41900cb814c4f2ef81a89cb004
TOMÁS GONZÁLEZ ATIENZAR <Tomas.Gonzalez2@alu.uclm.es>
2025-12-16 19:22:13 +0100

    we add the id to the option of the menu we want to select

--- test/AppForSEII2526.UIT/UC_Purchase/UC_PurchaseCars_UIT.cs
+++ test/AppForSEII2526.UIT/UC_Purchase/UC_PurchaseCars_UIT.cs
@@ -9,12 +9,22 @@ namespace AppForSEII2526.UIT.UC_Purchase
 {
     public class UC_PurchaseCars_UIT : UC_UIT 
     {
+        private SelectCarsForPurchase_PO selectCarsForPurchase_PO;
         public UC_PurchaseCars_UIT(ITestOutputHelper output) : base(output)
         {
         }
         private void Precondition_perform_login()
         {
             Perform_login("tomas.gonzalez@uclm.es", "OtherPass12$");
+        }
+
+        private void InitialStepsForPurchaseCars()
+        {
+            Precondition_perform_login();
+            //we wait for the option of the menu to be visible
+            selectCarsForPurchase_PO.WaitForBeingVisible(By.Id("CreatePurchase"));
+            //we click on the menu
+            _driver.FindElement(By.Id("CreatePurchase")).Click();
         }
     }
 }
