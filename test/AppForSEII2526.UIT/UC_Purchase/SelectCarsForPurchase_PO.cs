c5bffaa7f5dcc1f2a28972c3df604724ee6045ca
TOMÁS GONZÁLEZ ATIENZAR <Tomas.Gonzalez2@alu.uclm.es>
2025-12-16 19:12:06 +0100

    creating the PageObject to interact with SelectCarsForPurchase.

---
+++ test/AppForSEII2526.UIT/UC_Purchase/SelectCarsForPurchase_PO.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppForSEII2526.UIT.UC_Purchase
+{
+    public class SelectCarsForPurchase_PO : PageObject
+    {
+        By inputColor= By.Id("carColor");
+        By inputModel= By.Id("carModel");
+        By btnSearch= By.Id("searchCarsBtn");
+        public SelectCarsForPurchase_PO(IWebDriver driver,ITestOutputHelper output) : base(driver, output)
+        {
+        }
+        public void SearchCars(string model)
+        {
+            WaitForBeingClickable(inputModel);
+            _driver.FindElement(inputModel).SendKeys(model);
+            _driver.FindElement(btnSearch).Click();
+        }
+    }
+}
